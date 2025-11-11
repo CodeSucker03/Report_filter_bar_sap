b4870f32dbe30a260d579c7d7af494091e67a8dd
CodeSucker03 <khoinguyendao1210@gmail.com>
2025-11-11 16:57:17 +0700

    add dialog for adding

--- webapp/controller/DynamicPageListReport.controller.ts
+++ webapp/controller/DynamicPageListReport.controller.ts
@@ -583,6 +583,6 @@ export default class DynamicPageListReport extends Base {
      onCloseDialog(): void {
         // note: We don't need to chain to the pDialog promise, since this event-handler
         // is only called from within the loaded dialog itself.
-        (this.byId("helloDialog") as Dialog)?.close();
+        (this.byId("addDialog") as Dialog)?.close();
     } 
 }
